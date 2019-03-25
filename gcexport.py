8fb953522c3af2baff57e5275a98fe1013d250b3
Rob Alley <alley.rob@gmail.com>
2019-03-26 08:45:56 +1300

    Remove my changes that duplicate -ot functionality; move rename functionality into new -mv option

--- gcexport.py
+++ gcexport.py
@@ -22,8 +22,8 @@ from __future__ import print_function
 from datetime import datetime, timedelta, tzinfo
 from getpass import getpass
 from math import floor
-from os import mkdir, remove, stat, utime
-from os.path import isdir, isfile
+from os import mkdir, remove, stat, utime, renames
+from os.path import isdir, isfile, splitext
 from subprocess import call
 from timeit import default_timer as timer
 from urllib import urlencode
@@ -39,9 +39,7 @@ import urllib2
 import zipfile
 
 # RPA: Added to support changing fit file atime/ctime
-from fitparse import FitFile
 import time
-import os
 
 SCRIPT_VERSION = '2.1.4'
 
@@ -51,10 +49,6 @@ OPENER = urllib2.build_opener(urllib2.HTTPCookieProcessor(COOKIE_JAR))
 # this is almost the datetime format Garmin used in the activity-search-service
 # JSON 'display' fields (Garmin didn't zero-pad the date and the hour, but %d and %H do)
 ALMOST_RFC_1123 = "%a, %d %b %Y %H:%M"
-
-# RPA: Added to support changing fit file atime/ctime
-# Date and time format pattern used in fit file
-Fit_Date_Time = '%Y-%m-%d %H:%M:%S'
 
 # map the numeric parentTypeId to its name for the CSV output
 PARENT_TYPE_ID = {
@@ -376,6 +370,9 @@ def parse_arguments(argv):
     parser.add_argument('-ot', '--originaltime',
                         help="will set downloaded (and possibly unzipped) file time to the activity start time",
                         action="store_true")
+    parser.add_argument('-mv', '--move',
+                        help="move unzipped file to NEW directory",
+                        action="store_true")
     parser.add_argument('-t', '--template', nargs='?', default=CSV_TEMPLATE, help="template \
         file with desired columns for CSV output")
 
@@ -633,43 +630,17 @@ def export_data_file(activity_id, activity_details, activity_name, args, file_ti
                 for name in zip_obj.namelist():
                     unzipped_name = zip_obj.extract(name, args.directory)
                     if file_time:
+                        #print("Set file time for '%s'"%unzipped_name)
                         utime(unzipped_name, (file_time, file_time))
 
-                    fileNum = 0
-                    for name in zip_obj.namelist():
-                        zip_obj.extract(name, args.directory)
-
-                        # RPA: Added to support changing fit file atime/ctime
-                        # Set mtime and atime of fit file to the time of the activity
-                        full = args.directory + '/' + name
-                        if full.endswith(".fit"):
-                           # Count fit files from zip in case we end up with multiples
-                           fileNum += 1
-                        
-                           fitfile = FitFile(full)
-                           activity  = fitfile.get_messages('activity')
-                           timestamp = activity.next().get_value('timestamp')
-
-                           activityTime = time.strptime(str(timestamp), Fit_Date_Time)
-                           epoch = int(time.mktime(activityTime))
-
-                           newName = args.directory +'/NEW/' + time.strftime("%Y-%b-%d", activityTime) + "-" + str(fileNum) + "-" + activity_name + '.fit'
-                           print("Renaming fit file to '%s' and setting mtime/atime to %s" % (newName, timestamp))
-                           os.renames(full, newName)
-                           os.utime(newName, (epoch, epoch))
-
-                           # Recreate a dummy empty fit file so that we wont' re-download on subsequent runs
-                           # TODO: A better way would be to maintain a list of already downloaded files and don't download if it appears in the list
-                           fh = open(full, "wb")
-                           fh.close()
-
-                        else:
-                           print("WARNING: Not renaming or setting timestamp for file '%s'" % name)
-
-
-
-
-
+                    extension = splitext(unzipped_name)[1]
+                    if args.move:
+                       newName = args.directory +'/NEW/' + time.strftime("%Y-%b-%d", time.localtime(file_time)) + "_" + activity_name + "_" + activity_id + extension
+                       #print("Renaming file to '%s'" % newName)
+                       renames(unzipped_name, newName)
+                       # Recreate a dummy empty file so that we wont' re-download on subsequent runs
+                       fh = open(unzipped_name, "wb")
+                       fh.close()
 
                 zip_file.close()
             else:
